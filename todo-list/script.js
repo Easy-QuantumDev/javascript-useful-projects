6fec362c7ac48abca02c5b6e53b38238d04117ed
Easy-Quantum-Dev <mr7bbbbbbbb@gmail.com>
2026-08-10 12:59:47 +0430

    just work on the buttons and add task style

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,21 +1,42 @@
 const add_task_input = document.getElementById("task-input")
 const add_btn = document.getElementById("add-task")
 const todo_list = document.getElementById("todo-list")
+const error_alert_message = document.getElementById("error-message")
+
+
 
 add_btn.addEventListener("click", () => {
+    if (add_task_input.value.trim() === "") {
+        error_alert_message.textContent = "please enter a task !"
+        error_alert_message.style.display = "block"
+        return
+    }
+    error_alert_message.style.display = "none"
+
     let li = document.createElement("li")
-    const new_content = document.createTextNode(add_task_input.value)
-    li.appendChild(new_content)
+    let span = document.createElement("span")
+    li.classList.add('todo-item')
+    span.classList.add("todo-text")
+    const task_content = document.createTextNode(add_task_input.value)
+    span.appendChild(task_content)
+
+
+    li.appendChild(span)
     todo_list.appendChild(li)
-    
+
+
+
+    // ===============================
+
+
     const complete_btn = document.createElement("button")
     complete_btn.classList.add("todo-check")
-    const complete_btn_content = document.createTextNode(`complete ?`)
+    const complete_btn_content = document.createTextNode(`complete`)
     complete_btn.appendChild(complete_btn_content)
-    
+
     //================================= 
-    
-    
+
+
     const delete_btn = document.createElement("button")
     delete_btn.classList.add("delete-btn");
     const delete_btn_content = document.createTextNode(`delete`)
@@ -29,12 +50,24 @@ add_btn.addEventListener("click", () => {
     edit_btn.classList.add("edit-btn");
     const edit_btn_content = document.createTextNode(`edit`)
     edit_btn.appendChild(edit_btn_content)
-    
-    
+
+
     li.appendChild(complete_btn)
     li.appendChild(delete_btn)
     li.appendChild(edit_btn)
 
-
+    delete_btn.addEventListener("click", () => {
+        li.remove()
+    })
+    complete_btn.addEventListener("click", () => {
+        li.classList.toggle("completed")
+        edit_btn.disabled=true
+    })
     
+
 })
+
+
+
+
+
