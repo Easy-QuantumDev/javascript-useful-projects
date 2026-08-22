8bc5a38991ebc872f8590d172b0c9e3580e5fbf5
Easy-Quantum-Dev <mr7bbbbbbbb@gmail.com>
2026-08-22 20:19:56 +0430

    weather app completed

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -18,6 +18,12 @@ const feels_like = document.getElementById("feels-like")
 
 const unit_btn = document.getElementById("unit-btn")
 
+const forecast = document.getElementById("forecast")
+
+const loading = document.getElementById('loading')
+
+
+
 
 
 
@@ -28,64 +34,105 @@ async function SearchWeather() {
     let city = city_input.value.trim()
 
     if (city === "") {
-    
+
         error.textContent = "please enter a city"
-    
+
         error.style.display = 'block'
-    
+
         return
 
-    
+
     }
 
     error.style.display = 'none'
 
     try {
 
-
+        loading.style.display = "block"
+        search_btn.disabled = true
         const url =
-         `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`
+            `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`
 
 
+
+        const response = await fetch(url)
+
+        const data = await response.json()
+
+        const latitude = data["results"][0]["latitude"]
+
+        const longitude = data["results"][0]["longitude"]
+
+
+        const weather_url =
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`
+
+        const weather_response = await fetch(weather_url)
+
+        const weather_data = await weather_response.json()
+
+
+
+        const temperature_value = weather_data["current"]["temperature_2m"]
+        const humidity_value = weather_data["current"]["relative_humidity_2m"]
+        const feels_like_temp_value = weather_data["current"]["apparent_temperature"]
+        const wind_value = weather_data["current"]["wind_speed_10m"]
+        const weather_code = weather_data['current']['weather_code']
+        const description = getweatherdescription(weather_code)
+        const daily = weather_data['daily']
+
+
+
+
+
+        console.log(weather_data)
+        humidity.textContent = `${humidity_value}`
+        wind.textContent = `{wind_value} km/h `
+        feels_like.textContent = `${feels_like_temp_value}°C `
+        temperature.textContent = temperature_value
+        city_name_seached.textContent = data['results'][0]['name']
+        weather_description.textContent = description
+        forecast.innerHTML = ""
+        for (let i = 0; i < daily['time'].length; i++) {
+            const card = document.createElement("div")
+            card.classList.add("forecast-card")
+            const date = new Date(daily["time"][i])
+            const day = date.toLocaleDateString("en-US", {
+                weekday: "short"
+            })
+            const max_temp = daily["temperature_2m_max"][i]
+
+            const code = daily["weather_code"][i]
+
+            const description = getWeatherDescription(code)
+            const icon = getWeatherIcon(code)
+            card.innerHTML = `<span>${i === 0 ? "Today" : day}</span>
+
+        <i class="${icon}"></i>
         
-            const response = await fetch(url)
-        
-            const data = await response.json()
-        
-            const latitude = data["results"][0]["latitude"]
-        
-            const longitude = data["results"][0]["longitude"]
+        <strong>${Math.round(max_temp)}°</strong>
 
-        
-            const weather_url =
-             `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m`
+        <small>${description}</small>
+    `
+            forecast.appendChild(card)
+        }
 
 
-       
-            const weather_response = await fetch(weather_url)
-       
-            const weather_data = await weather_response.json()
-
-            const temperature = weather_data["current"]["temperature_2m"]
-            const humidity = weather_data["current"]["relative_humidity_2m"]
-            const feels_like_temp = weather_data["current"]["apparent_temperature"]
-            const wind = weather_data["current"]["wind_speed_10m"]
-
-
-
-            console.log(weather_data)
-  
     }
 
 
     catch (err) {
-       
+
         console.log(err)
-       
+
         error.textContent = 'sorry the service is not avalable'
-       
+
         error.style.display = 'block'
-    
+
+    }
+    finally{
+        loading.style.display = "none"
+        search_btn.disabled = false
     }
 
 
@@ -93,5 +140,79 @@ async function SearchWeather() {
 
 }
 
+
+function getweatherdescription(code) {
+
+    if (code === 0) {
+        return "Clear Sky ☀️"
+    }
+
+    if (code === 1 || code === 2) {
+        return "Partly Cloudy 🌤️"
+    }
+
+    if (code === 3) {
+        return "Overcast ☁️"
+    }
+
+    if (code >= 45 && code <= 48) {
+        return "Foggy"
+    }
+
+    if (code >= 51 && code <= 67) {
+        return "Rain 🌧️"
+    }
+
+    if (code >= 71 && code <= 77) {
+        return "Snow ❄️"
+    }
+
+    if (code >= 80 && code <= 82) {
+        return "Rain Showers"
+    }
+
+    if (code >= 95) {
+        return "Thunderstorm ⛈️"
+    }
+
+    return "Unknown"
+}
+
+function getWeatherIcon(code) {
+
+    if (code === 0) {
+        return "fa-solid fa-sun"
+    }
+
+    if (code === 1 || code === 2) {
+        return "fa-solid fa-cloud-sun"
+    }
+
+    if (code === 3) {
+        return "fa-solid fa-cloud"
+    }
+
+    if (code >= 45 && code <= 48) {
+        return "fa-solid fa-smog"
+    }
+
+    if (code >= 51 && code <= 67) {
+        return "fa-solid fa-cloud-rain"
+    }
+
+    if (code >= 71 && code <= 77) {
+        return "fa-solid fa-snowflake"
+    }
+
+    if (code >= 80 && code <= 82) {
+        return "fa-solid fa-cloud-showers-heavy"
+    }
+
+    if (code >= 95) {
+        return "fa-solid fa-cloud-bolt"
+    }
+
+    return "fa-solid fa-cloud"
+}
 
 search_btn.addEventListener("click", SearchWeather)
